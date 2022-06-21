47562536e94282b0b232c2468613f49a8432dd69
ldruwel <laurens.druwel@jimber.org>
2022-06-21 13:57:01 +0200

    implementation of audiostreamer

--- public/script.js
+++ public/script.js
@@ -1,33 +1,57 @@
+getFiles = async (setlist = "") => {
+  libaryURL = "http://localhost:3000/library";
+  if (setlist !== "") {
+    libaryURL = libaryURL + "/" + setlist;
+  }
+  res = await fetch(libaryURL);
+  return await res.json();
+};
 
-
-getLibrary= async() =>{
-        let res = await fetch("http://localhost:3000/library")
-        return await res.json()
+loadFiles = async (setlist) => {
+  document.getElementById("setlist").innerHTML = setlist;
+  getFiles(setlist).then(
+    function (data) {
+      let songs = "<ul>";
+      for (i = 0; i < data.length; i++) {
+        songs +=
+          "<li><button onclick=startStreaming('" +
+          data[i] +
+          "')>" +
+          data[i] +
+          "</button></li>";
+      }
+      songs += "</ul>";
+      document.getElementById("fileBrowser").innerHTML = songs;
+    },
+    function (error) {
+      console.err(error);
     }
+  );
+};
 
+startStreaming = async (song) => {
+  console.log("START STREAM " + song);
 
-  loadLibrary = async()=>{    
-    getLibrary().then(
-        function(data){
-            let setlist = "<ul>";
+  result = await fetch("http://localhost:3000/stream/"
+     + document.getElementById("setlist").innerHTML + "/" + song);
+  document.getElementById("player").src = result.url;
+};
 
-            for(i=0; i< data.length;i++){
-                setlist+= "<li><button>"+data[i]+"</button></li>"
-        
-            }
-            setlist += "</ul>"
-            document.getElementById("setlist").innerHTML = setlist;
-        },
-        function(error){
-            console.log(error)
-        }
-    );  
-}
+loadLibrary = async () => {
+  getFiles().then(
+    function (data) {
+      let setlist = "<ul>";
 
-
-
-loadLibrary();
-
-
-
-
+      for (i = 0; i < data.length; i++) {
+        files = data[i];
+        setlist +=
+          "<li><button onclick=loadFiles('" + data[i] + "')>" + data[i] + "</button></li>";
+      }
+      setlist += "</ul>";
+      document.getElementById("fileBrowser").innerHTML = setlist;
+    },
+    function (error) {
+      console.err(error);
+    }
+  );
+};
--- server.js
+++ server.js
@@ -9,42 +9,56 @@ const audioPath = "./audio";
 
 app.use(express.static("public"));
 
-const filePath = "./copyrightFree/";
-
-
-
 app.get("/library/:subfolder?", (req, res) => {
-    if(req.params.subfolder){
-        tmp = audioPath +"/" + req.params.subfolder;
-        fs.readdir(tmp, (err, mp3s) =>{
-            console.log(mp3s)
-            res.writeHead(200, {'Content-Type': 'audio/mpeg'});
-            res.write(JSON.stringify(mp3s));
-            return res.send();
-          })
-    }
-    else{
-        tmp = audioPath +"/" + req.params.subfolder;
-        fs.readdir(audioPath, (err, dirs) =>{
-            console.log(dirs)
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.write(JSON.stringify(dirs));
-            return res.send();
-          })
-    }
-    
-    
-        
-    
-
-
-
-    
-
+  if (req.params.subfolder) {
+    fullPath = audioPath + "/" + req.params.subfolder;
+    fs.readdir(fullPath, (err, mp3s) => {
+      res.writeHead(200, { "Content-Type": "audio/mpeg" });
+      res.write(JSON.stringify(mp3s));
+      return res.send();
+    });
+  } else {
+    fullPath = audioPath + "/" + req.params.subfolder;
+    fs.readdir(audioPath, (err, data) => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.write(JSON.stringify(data));
+      return res.send();
+    });
+  }
 });
 
+app.get("/stream/:setlistName/:songTitle", function (req, res) {
+  songTitle = req.params.songTitle;
+  setlistName = req.params.setlistName;
+  range = req.headers.range;
 
+  audioFile = "./audio/" + setlistName + "/" + songTitle;
+  stat = fs.statSync(audioFile);
+
+  var audioReadStream;
+
+  if (range !== undefined) {
+    endBytes = stat.size - 1;
+    res.status(206).header({
+      "Content-Type": "audio/mpeg",
+      "Content-Length": stat.size,
+      "Content-Range": "bytes " + 0 + "-" + endBytes + "/" + stat.size,
+    });
+
+    audioReadStream = fs.createReadStream(
+      (path = audioFile),
+      (options = { end: endBytes })
+    );
+  } else {
+    res.header({
+      "Content-Type": "audio/mpeg",
+      "Content-Length": stat.size,
+    });
+    audioReadStream = fs.createReadStream((path = audioFile));
+  }
+  audioReadStream.pipe(res);
+});
 
 app.listen(port, () => {
-    console.log(`Navigate to http://localhost:${port}`)
+  console.log(" -> http://localhost:"+ port +";")
 });
